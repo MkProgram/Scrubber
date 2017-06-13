f3f4127b701f3819068350c55f4ffc9838d4a670
Lea <lea.stoeckel@gmail.com>
2017-06-13 22:29:45 +0200

    register components in app.component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { TicketComponent } from './components/ticket/ticket.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    TicketComponent
+    TicketComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
