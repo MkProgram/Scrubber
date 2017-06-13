fc310c9eeddd6a0ff49f4ed22c0f1c1a37ff5321
Jonas Fischer <jonasfischer@minint-0qo0tsn.bs14.lan>
2017-06-13 14:41:59 +0200

    ticket animation added

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -12,7 +13,8 @@ import { TicketComponent } from './components/ticket/ticket.component';
     TicketComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
