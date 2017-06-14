3874bc789cca45aef02606290944d20cb364cbdd
Lea <lea.stoeckel@gmail.com>
2017-06-14 11:50:54 +0200

    added addTicket function

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ITicket } from './components/addTicket/addTicket.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  tickets:Array<ITicket> = [];
 }
