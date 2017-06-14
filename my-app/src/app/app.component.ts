d5084ea2f618d1406852981a0b86be82d8b87578
Lea <lea.stoeckel@gmail.com>
2017-06-14 12:40:55 +0200

    added removeTickets

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,7 +6,13 @@ import { ITicket } from './components/addTicket/addTicket.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
   tickets:Array<ITicket> = [];
+
+  removeTicket(uuid:string) {
+    const ticketIndex = this.tickets.findIndex((ticket)=>ticket.id===uuid);
+    this.tickets.splice(ticketIndex, 1);
+  }
 }
